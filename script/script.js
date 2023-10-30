93792a21bc75b5bef244a972d4ccbbe95f81aebd
Wesley Medeiros <da.rosa.wesley25@gmail.com>
2023-10-30 18:04:26 -0300

    Projeto em andamento

--- script/script.js
+++ script/script.js
@@ -1,50 +1,66 @@
 $(function(){
-    const buttonX = $('.buttons > #btnX')
-    const buttonO = $('.buttons > #btnO')
     const cells = $('.cell').toArray()
-
+    let textoJogadaX = document.getElementById('btnX')
+    let textoJogadaO = document.getElementById('btnO')
     // Função que cria um elemento HTML X
     const elementHTMLX = () => {
         let elementHtmlImgx = document.createElement("img")
         elementHtmlImgx.setAttribute("class", "imageX")
-        elementHtmlImgx.setAttribute("src", "../assets/x.png")
+        elementHtmlImgx.setAttribute("src", "../assets_arquivos/x.png")
         elementHtmlImgx.setAttribute("alt", "Imagem_X")
+        elementHtmlImgx.style.display = 'block'
+        return elementHtmlImgx
     }
     // Função que cria um elemento HTML O
     const elementHTMLO = () => {
         let elementHtmlImgo = document.createElement("img")
         elementHtmlImgo.setAttribute("class", "imageO")
-        elementHtmlImgo.setAttribute("src", "../assets/circle.png")
+        elementHtmlImgo.setAttribute("src", "../assets_arquivos/circle.png")
         elementHtmlImgo.setAttribute("alt", "Imagem_O")
+        elementHtmlImgo.style.display = 'block'
+        return elementHtmlImgo
     }
     // Função que verifica se uma célula está vazia ou com o valor X
-    const cellsEmptyValidationX = ()=>{
+    const cellsEmptyValidationX = (cell) => {
         if ($('.cell').is(':empty')) {
-            $('.cell').append(elementHTMLX())
-        } else {
-            if($('.cell').find('img.imageO').length === 0) {
-                $('.cell').append(elementHTMLX())
-            } else {
-                alert(`Célula já preenchida com o valor X`)
-            }
+            let iconX = elementHTMLX();
+            $('.cell').append(iconX);
+        } else if ($('.cell').find('img.imageO').length === 1) {
+            alert(`Célula já preenchida com o valor O`);
         }
     }
+    
     // Função que verifica se uma célula está vazia ou com o valor O
-    const cellsEmptyValidationO = ()=>{
+    const cellsEmptyValidationO = (cell) => {
         if ($('.cell').is(':empty')) {
-            $('.cell').append(elementHTMLO())
-        } else {
-            if($('.cell').find('img.imageX').length === 0) {
-                $('.cell').append(elementHTMLO())
-            } else {
-                alert(`Célula já preenchida com o valor O`)
-            }
+            let iconO = elementHTMLO();
+            $('.cell').append(iconO);
+        } else if ($('.cell').find('img.imageX').length === 1) {
+            alert(`Célula já preenchida com o valor X`);
         }
     }
-    // Iterando sobre o array de células
-    $.each(cells, function(index, element){
-        $(element).on('click', ()=>{
-            cellsEmptyValidationX()
-        })
-    })
+    textoJogadaX.addEventListener("click", () => {
+        JogadorAtual = 'X';
+        document.querySelector("#textoJogada").textContent = 'Jogada X';
+    });
+    
+    textoJogadaO.addEventListener("click", () => {
+        JogadorAtual = 'O';
+        document.querySelector("#textoJogada").textContent = 'Jogada O';
+    });
+    
+    $.each(cells, function (index, element) {
+        $(element).on('click', () => {
+            if (JogadorAtual === 'X') {
+                cellsEmptyValidationX(element);
+                JogadorAtual = 'O';
+                document.querySelector("#textoJogada").textContent = 'Jogada O';
+            } else if (JogadorAtual === 'O') {
+                cellsEmptyValidationO(element);
+                JogadorAtual = 'X';
+                document.querySelector("#textoJogada").textContent = 'Jogada X';
+            }
+        });
+    });
+    
 });
